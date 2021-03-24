9d55ee9f0316829726c8a73f53f4bacb9b7a311c
MSumaiya <mirza01.alam@gmail.com>
2021-03-24 01:40:18 +0100

    Co-authored-by: Protik Sarkar <sarkar.protik1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,90 +1,24 @@
-import React, {useState} from 'react';
-import QrReader from 'react-qr-reader';
-import JsonInfo from './JsonInfo.js';
-import './App.css';
-/* class App extends Component {
-  state = {
-    result: 'No result',
-    qrResponse:{}
-  }
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-  handleScan = data => {
-    if (data) {
-      this.setState({
-        result: data,
-        qrResponse:JSON.parse(data)
-      })
-      console.log(data.CageId);
-    }
-  }
-  handleError = err => {
-    console.error(err)
-  }
-  render() {
-    return (
-      <div>
-        <QrReader
-          delay={300}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '60%' }}
-        />
-        <p>{this.state.result}</p>
-        <p>{this.state.qrResponse.CageId}</p>
-      
-      </div>
-    )
-  }
-} */
-/* export default function App(){
+import MainOne from './main'
 
-  const handleScan = (data) => {
-      console.log(data);
-
-      alert("Got scan: " + data);
-  }
-
-  const handleError = (error) => {
-      console.log("Error: ", error);
-  }
-
-  return <QrReader delay={500} onScan={handleScan} onError={handleError}></QrReader>
-  
-} */
-
+const SomethingElse = () => <div>Something Else</div>
 export default function App(){
-  const [qrValue, setQrValue] = useState({});
-
-  //const hello = 'Hello World';
-
-  const handleScan = (data) => {
-    if (data) {
-      setQrValue(JSON.parse(data));
- 
-      doSomethingWithCage(JSON.parse(data).CageId);
-    }
-  };
- 
-  const handleError = (error) => {
-    console.log("Error: ", error);
-  };
- 
-  const doSomethingWithCage = (cageId) => {
-    alert("I did something with cage: " + cageId);
-    
-  };
- 
   return (
-    <>
-      <QrReader
-        delay={500}
-        onScan={handleScan}
-        onError={handleError}
-        style={{width:'50%', height:'50%'}}
-      ></QrReader>
-      <p>{JSON.stringify(qrValue)}</p>
-      <p>CageId: {qrValue.CageId}</p>
-      <JsonInfo data={qrValue.CageId}/>
-    </>
-  );
-};
+  <Router>
+    <div>
+      <Link to="/">Home</Link>{' '}
+      <Switch>
+        <Route exact path="/" component={MainOne} />
+        <Route exact path="/data" component={SomethingElse} />
+      </Switch>
+      </div>
+  </Router>)
+  
+}
---
+++ src/main.js
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import QrReader from 'react-qr-reader';
+import JsonInfo from './JsonInfo.js';
+import './App.css';
+/* class App extends Component {
+  state = {
+    result: 'No result',
+    qrResponse:{}
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data,
+        qrResponse:JSON.parse(data)
+      })
+      console.log(data.CageId);
+    }
+  }
+  handleError = err => {
+    console.error(err)
+  }
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '60%' }}
+        />
+        <p>{this.state.result}</p>
+        <p>{this.state.qrResponse.CageId}</p>
+      
+      </div>
+    )
+  }
+} */
+/* export default function App(){
+
+  const handleScan = (data) => {
+      console.log(data);
+
+      alert("Got scan: " + data);
+  }
+
+  const handleError = (error) => {
+      console.log("Error: ", error);
+  }
+
+  return <QrReader delay={500} onScan={handleScan} onError={handleError}></QrReader>
+  
+} */
+
+const MainOne = () => {
+  const [qrValue, setQrValue] = useState({});
+
+  //const hello = 'Hello World';
+
+  const handleScan = (data) => {
+    if (data) {
+      setQrValue(JSON.parse(data));
+ 
+      doSomethingWithCage(JSON.parse(data).CageId);
+    }
+  };
+ 
+  const handleError = (error) => {
+    console.log("Error: ", error);
+  };
+ 
+  const doSomethingWithCage = (cageId) => {
+    alert("I did something with cage: " + cageId);
+    
+  };
+  const [state, setState] = useState({data:""}) 
+  const changeState = () => {   
+    setState({data: qrValue.CageId});  
+   };  
+
+  console.log(state)
+  return (
+    <>
+      <QrReader
+        delay={500}
+        onScan={handleScan}
+        onError={handleError}
+        style={{width:'50%', height:'50%'}}
+      ></QrReader>
+      <p>{JSON.stringify(qrValue)}</p>
+      <p>CageId: {qrValue.CageId}</p>
+      <JsonInfo data={qrValue.CageId}/>
+      <button  onClick={changeState} type="button"> 
+                  Send state  
+                  {console.log(state)}
+                </button>     
+    </>
+  )
+}
+
+export default MainOne;
